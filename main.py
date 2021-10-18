313022b6da8ba841a3fbef825f782c780e12dbe2
erik-dunteman <erikdunteman@gmail.com>
2021-10-19 01:29:16 +0200

    first refactor and clean

--- main.py
+++
@@ -1,7 +0,0 @@
-import booste
-
-out = booste.clip(
-    api_key = "f1f22e45-8ae6-4658-911c-cb015014cc03", 
-    prompts = ["A blue and black dress", "A gold and white dress"], 
-    images = ["https://en.wikipedia.org/wiki/The_dress#/media/File:The_Dress_(viral_phenomenon).png"], 
-    pretty_print=True)
--- proto.py
+++
@@ -1,5 +0,0 @@
-
-print("\n\t\tPrompt:\t\t", "A handsome man", "\n\t\tImage URL:\t", "./johnny.jpg", "\n\t\tSimilarity:\t", "100", "")
-print("\n\t\tPrompt:\t\t", "An average looking man", "\n\t\tImage URL:\t", "./johnny.jpg", "\n\t\tSimilarity:\t", "0", "")
-print("\n\t\tPrompt:\t\t", "An ugly man", "\n\t\tImage URL:\t", "./johnny.jpg", "\n\t\tSimilarity:\t", "0", "")
-print("\n")
